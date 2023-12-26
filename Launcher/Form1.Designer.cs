3aefb7509bc46d62a86e328450a6cd03c3228eb1
AmirHesam <62202741+AmirHesam-Python@users.noreply.github.com>
2023-12-26 17:27:58 +0330

    Add files via upload

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.kir = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start The Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(555, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(209, 360);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(201, 334);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Usage";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(6, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 92);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(6, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(6, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "system usage:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(6, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(201, 334);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Setting\'s";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(230, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "ping";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // kir
+            // 
+            this.kir.Location = new System.Drawing.Point(35, 1);
+            this.kir.Name = "kir";
+            this.kir.Size = new System.Drawing.Size(127, 12);
+            this.kir.TabIndex = 4;
+            this.kir.Text = "label3";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(92, 330);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(393, 21);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Launcher Made By: Amir Hesam @Raitin Copy Right 2023\r\n";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(233, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 44);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "OverGame";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = global::Launcher.Properties.Resources.wallpaperflare_com_wallpaper__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(761, 360);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.kir);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "[O]ver[G]ame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label kir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Launcher/Form1.cs
@@ -0,0 +1,254 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using System.Security.Principal;
+using System.Xml.Linq;
+using System.Net.NetworkInformation;
+using System.Management;
+
+
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Runtime.CompilerServices;
+
+namespace Launcher
+{
+    
+    public partial class Form1 : Form
+    {
+
+        
+        
+
+
+
+
+
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+        public Form1()
+        {
+            InitializeComponent();
+            InitialiseCPUCounter();
+            InitializeRAMCounter();
+            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kir.Text = "Welcome" + " " + Environment.UserName;
+            string hwid = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001", "HwProfileGuid", null).ToString();
+
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\OverGame");
+            key.SetValue("UserName", textBox1);
+            key.SetValue("Setting2", "Status:");
+            key.Close();
+            label5.Text = "HwID:" + " " + hwid;
+
+            
+                                                                                                               
+
+        }
+        private void InitialiseCPUCounter()
+        {
+            cpuCounter = new PerformanceCounter(
+            "Processor",
+            "% Processor Time",
+            "_Total",
+            true
+            );
+        }
+
+        private void InitializeRAMCounter()
+        {
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes", true);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ServerIP = "127.0.0.1";
+            
+            string GTAPath = Registry.CurrentUser.OpenSubKey(@"Software\\SAMP").GetValue("gta_sa_exe").ToString();
+            GTAPath = GTAPath.Substring(0, GTAPath.LastIndexOf(@"\") + 1);
+            string[] ImaCheat1 = Directory.GetDirectories(GTAPath, "cleo");
+            string[] ImaCheat2 = Directory.GetFiles(GTAPath, "CLEO.asi");
+            string[] ImaCheat3 = Directory.GetFiles(GTAPath, "d3d9.dll");
+            string[] ImaCheat4 = Directory.GetFiles(GTAPath, "SAMPFUNCS.asi");
+            string[] ImaCheat5 = Directory.GetDirectories(GTAPath, "mod_sa");
+            string[] ImaCheat6 = Directory.GetDirectories(GTAPath, "OverLight_Mod");
+            string[] ImaCheat7 = Directory.GetFiles(GTAPath, "vorbishooked.dll");
+            string[] ImaCheat8 = Directory.GetFiles(GTAPath, "modloader");
+            string[] ImaCheat9 = Directory.GetFiles(GTAPath, "CustomSAA2");
+            string[] ImaCheat10 = Directory.GetFiles(GTAPath, "ImVehFt");
+     
+            string[] ImaCheat12 = Directory.GetFiles(GTAPath, "moadloader.asi");
+            string[] ImaCheat13 = Directory.GetFiles(GTAPath, "DSOUND.dll");
+
+            if (((ImaCheat1.Length > 0) || (ImaCheat2.Length > 0)) || ((ImaCheat3.Length > 0) || (ImaCheat4.Length > 0)))
+            {
+                MessageBox.Show("Ilegal Mod Detected", "Ilegal Mod Detected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if ((ImaCheat5.Length > 0) || (ImaCheat6.Length > 0))
+            {
+                MessageBox.Show("Ilegal Mod Detected", "Ilegal Mod Detected", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat7.Length < 0)
+            {
+                MessageBox.Show("Launcher Error", "You Are Not Using Launcher", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat8.Length < 0)
+            {
+                MessageBox.Show("Launcher Error", "You Are Not Using Launcher", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat9.Length < 0)
+            {
+                MessageBox.Show("Launcher Error", "You Are Not Using Launcher", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat9.Length < 0)
+            {
+                MessageBox.Show("Launcher Error", "You Are Not Using Launcher", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat10.Length < 0)
+            {
+                MessageBox.Show("Launcher Error", "You Are Not Using Launcher", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat12.Length < 0)
+            {
+                MessageBox.Show("Launcher Error", "You Are Not Using Launcher", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat13.Length < 0)
+            {
+                MessageBox.Show("Launcher Error", "You Are Not Using Launcher", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else
+            {
+               Registry.CurrentUser.OpenSubKey(@"Software\SAMP", true).SetValue("PlayerName", textBox1.Text);
+                Process.Start(GTAPath + "samp.exe", ServerIP + "");
+
+            }
+            
+
+    }
+
+       private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            
+            //System.IO.File.WriteAllText(@"C:\Users\Ariyanpishro\Documents\GTA San Andreas User Files\SAMP\sa-mp.cfg", data);
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+
+
+
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = "CPU Usage: " +
+            Convert.ToInt32(cpuCounter.NextValue()).ToString() + "%";
+
+
+            label3.Text = "RAM Usage:" + " " + Convert.ToInt32(ramCounter.NextValue()).ToString() + "Mb";
+
+
+            // Print the user name to the console
+            Ping p = new Ping();
+            PingReply r;
+            string s;
+            s = "127.0.0.1";
+            r = p.Send(s);
+
+            if (r.Status == IPStatus.Success)
+            {
+                label6.Text = "Ping Server: "+  r.RoundtripTime.ToString() + " ms" + "\n";
+            }
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
